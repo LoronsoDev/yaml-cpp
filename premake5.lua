5e6917bfb9fb892a392f10d510ef0b86bcbaa5b6
LorenzoHerran <lorenzohs98@gmail.com>
2023-09-20 17:06:11 +0200

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "yaml-cpp"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir("bin/" .. ouputdir .."/%{prj.name}")
+    objdir("bin-int/" ..outputdir .. "%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+
+        "include/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter "system:linux"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
